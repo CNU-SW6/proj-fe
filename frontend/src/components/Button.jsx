cfccfd5f0c2d4fc49374a4d2ea9477eab07d04f4
narlo <aks152312@gmail.com>
2023-01-16 15:26:02 +0900

    세팅 수정

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -8,7 +8,12 @@ import '../styles/button.css';
 export function Button({ primary, backgroundColor, size, label, ...props }) {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button type="button" className={['storybook-button', `storybook-button--${size}`, mode].join(' ')} style={backgroundColor && { backgroundColor }} {...props}>
+    <button
+      type="button"
+      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
       {label}
     </button>
   );
